ce8ed39c632cd0f13f65accb1ebc7877eee15ab7
Erpriex <erpriex@gmail.com>
2021-12-04 20:12:12 +0100

    Change variable name

--- src/main/java/fr/askhim/api/controllers/HelloController.java
+++ src/main/java/fr/askhim/api/controllers/HelloController.java
@@ -2,14 +2,16 @@ package fr.askhim.api.controllers;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 	
-	@RequestMapping("/hello")
-	public String hello(HttpServletResponse httpServletResponse) {
+	@GetMapping
+	public String helloGet(HttpServletResponse response) {
 		return "Hello world !";
 	}
 
