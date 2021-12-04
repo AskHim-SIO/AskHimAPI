10689a7139bfa141231c625e1981ba327d0347b4
Erpriex <erpriex@gmail.com>
2021-12-04 19:34:48 +0100

    First commit

---
+++ src/main/java/fr/askhim/api/AskHimApiApplication.java
@@ -0,0 +1,13 @@
+package fr.askhim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AskHimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AskHimApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/askhim/api/AskHimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.askhim.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AskHimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
