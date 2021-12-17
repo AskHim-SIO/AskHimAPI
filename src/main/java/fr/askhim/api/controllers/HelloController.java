fc4546f75a34313fe607b39bb1b7ad2de02f3e84
Erpriex <erpriex@gmail.com>
2021-12-17 16:54:49 +0100

    Ecriture db en cours

--- src/main/java/fr/askhim/api/controllers/HelloController.java
+++ src/main/java/fr/askhim/api/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package fr.askhim.api.controllers;
 
+import fr.askhim.api.services.AskHimUserService;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,16 @@ import fr.askhim.api.AskHimUser;
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+
+	private AskHimUserService userService;
+
+	public HelloController(AskHimUserService userService){
+		this.userService = userService;
+	}
 	
 	@PostMapping
-	public String helloGet(@RequestBody AskHimUser user, String test) {
+	public String helloGet(@RequestBody AskHimUser user) {
+		userService.registerUser(user.nom, user.prenom);
 		return "Hello " + user;
 	}
 
--- src/main/java/fr/askhim/api/services/AskHimUserService.java
+++ src/main/java/fr/askhim/api/services/AskHimUserService.java
@@ -2,11 +2,14 @@ package fr.askhim.api.services;
 
 import org.springframework.stereotype.Service;
 
+import fr.askhim.api.models.entity.AskHimUserEntity;
+
 @Service
 public class AskHimUserService {
 	
 	public void registerUser(String nom, String prenom) {
 		
+		AskHimUserEntity entityToDB = AskHimUserEntity.builder().nom(nom).prenom(prenom).build();
 		
 	}
 
