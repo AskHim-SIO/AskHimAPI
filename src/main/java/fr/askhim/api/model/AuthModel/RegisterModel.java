75024ca031ce08b44fa566b8f40842a8ab358904
malco <malcom.morel@gmail.com>
2022-01-22 15:22:41 +0100

    correction modele

--- src/main/java/fr/askhim/api/model/AuthModel/RegisterModel.java
+++ src/main/java/fr/askhim/api/model/AuthModel/RegisterModel.java
@@ -3,6 +3,8 @@ package fr.askhim.api.model.AuthModel;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 @Getter
 @Setter
 public class RegisterModel {
@@ -11,4 +13,5 @@ public class RegisterModel {
     private String firstname;
     private String email;
     private String password;
+    private Date dateNaiss;
 }
--- src/main/java/fr/askhim/api/model/ServiceMinModel.java
+++ src/main/java/fr/askhim/api/model/ServiceMinModel.java
@@ -1,10 +1,12 @@
 package fr.askhim.api.model;
 
 
+import fr.askhim.api.models.entity.Lieu;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 
@@ -15,5 +17,7 @@ public class ServiceMinModel {
     private Long id;
     private String name;
     private Integer price;
+    private Date postDate;
+    private Lieu lieu;
     private List<PhotoModel> photos = new ArrayList<>();
 }
